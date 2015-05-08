bd26d193754fe539184b3b2f2557666917ae6889
Adam Rothschild <thirdtriplet@gmail.com>
2015-05-08 15:29:38 -0400

    Initial generated application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GroupsyDaisy::Application.config.secret_token = '8853a0a089db206b6c60379a2358782dec59a114734cf86112f3db3089d800090b0e0a7f69b288213a897bd08949c998225e62b266b3f16aa4e382ef986dc6a4'
